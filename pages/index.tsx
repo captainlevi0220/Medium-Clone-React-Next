737e2772d79cd42b0bb23854fd2f5ff81c69a9b4
captainlevi0220 <captainlevi0220@protonmail.com>
2022-04-02 01:46:00 -0400

    partway through implementing posts

--- pages/index.tsx
+++ pages/index.tsx
@@ -11,8 +11,7 @@ interface Props {
 }
 
 // const Home: NextPage = ({posts}: Props) => {
-export default function Home({posts}: Props) {
-
+export default function Home({ posts }: Props) {
   console.log(posts)
   return (
     <div className="mx-auto max-w-7xl">
@@ -20,9 +19,8 @@ export default function Home({posts}: Props) {
         <title>Medium Blog</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-}
+      }
       <Header />
-
       <div className="flex items-center justify-between border-y border-black bg-yellow-400 py-10 lg:py-0">
         <div className="space-y-5 px-10">
           <h1 className="max-w-xl font-serif text-6xl">
@@ -40,18 +38,28 @@ export default function Home({posts}: Props) {
         />
       </div>
       {/* Posts */}
-      <div>
+      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-3 md:gap-6 p-2 md:p-6">
         {posts.map((post) => (
           <Link key={post._id} href={`/post/${post.slug.current}`}>
-            <div>
-              <img src={urlFor(post.mainImage).url()!} alt="" />
-              <div>
+            <div className="group cursor-pointer">
+              <img
+                className="h-60 w-full object-cover group-hover:scale-105 transition-transform duration-200 ease-in-out"
+                src={urlFor(post.mainImage).url()!}
+                alt=""
+              />
+              <div className="flex justify-between p-5 bg-white">
                 <div>
                   <p>{post.title}</p>
-                  <p>{post.description} by {post.author.name}</p>
+                  <p>
+                    {post.description} by {post.author.name}
+                  </p>
                 </div>
 
-                <img src={urlFor(post.author.image).url()!} alt='' />
+                <img
+                  className="h-12 w-12 rounded-full"
+                  src={urlFor(post.author.image).url()!}
+                  alt=""
+                />
               </div>
             </div>
           </Link>
