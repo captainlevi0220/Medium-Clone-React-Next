35274715e89e51df96e493573c1b0a21b9162f66
captainlevi0220 <captainlevi0220@protonmail.com>
2022-04-02 03:51:00 -0400

    still working on posts

---
+++ pages/post/[slug].tsx
@@ -0,0 +1,16 @@
+import Header from '../../components/Header'
+import { sanityClient, urlFor } from '../../sanity'
+
+function Post() {
+  return (
+    <main>
+      <Header />
+    </main>
+  )
+}
+
+export default Post
+
+export const getStaticPaths = async () => {
+  const query = ``
+}
